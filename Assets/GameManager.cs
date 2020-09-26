9b0b1d1fc01ba6131fa9aacf33e03fa8610adfdd
DeonMartinez <49213704+DeonMartinez@users.noreply.github.com>
2020-09-25 23:30:27 -0700

    Add files via upload

---
+++ Assets/GameManager.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class GameManager : MonoBehaviour
+{
+
+    bool gameOver = false;
+
+    public float restartDelay = 2f;
+
+    public GameObject completeLevelUI;
+
+    //half way marker text
+    public Text halfWayText;
+
+    private void OnEnable()
+    {
+        PlayerMovement.OnHalfWay += DisplayText;
+    }
+
+    private void OnDisable()
+    {
+        PlayerMovement.OnHalfWay -= DisplayText;
+    }
+    public void CompleteLevel()
+    { 
+        completeLevelUI.SetActive(true);
+    }
+
+    public void EndGame()
+    {
+        if (gameOver == false) {
+            gameOver = true;
+            Invoke("Restart", restartDelay);
+        }
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void DisplayText()
+    {
+        halfWayText.text = "You're Doing Great";
+    }
+}
